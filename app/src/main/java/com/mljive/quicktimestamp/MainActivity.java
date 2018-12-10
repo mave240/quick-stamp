2a95afa80e565a4fcb02b30fc5622f0c0182735b
mave240 <mave240@gmail.com>
2018-12-10 22:58:50 +0000

    working for now

--- app/src/main/java/com/mljive/quicktimestamp/MainActivity.java
+++ app/src/main/java/com/mljive/quicktimestamp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.mljive.quicktimestamp;
 
-import android.app.*;
-import android.os.*;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
 
-public class MainActivity extends Activity 
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+	/**
+	 * AppWidgetSample demonstrates app widgets, including:
+	 * - Creating a template app widget to an app.
+	 * - Updating the app widget periodically (every 30 minutes).
+	 * - Adding a button to the app widget that updates on demand.
+	 * <p>
+	 * MainActivity is unused and includes a message to add the app widget to
+	 * the user's home screen.
+	 */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+	public void onNewLabelButtonClick(View view) 
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+        finish();
+	}
 }
